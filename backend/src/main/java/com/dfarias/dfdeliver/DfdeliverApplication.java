da6287490c3016a9dd03e19e1f6bba0ab990c2db
DaviFarias7 <davic.farias7@gmail.com>
2022-03-22 18:22:32 -0300

    Projeto criado

---
+++ backend/src/main/java/com/dfarias/dfdeliver/DfdeliverApplication.java
@@ -0,0 +1,13 @@
+package com.dfarias.dfdeliver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DfdeliverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DfdeliverApplication.class, args);
+	}
+
+}

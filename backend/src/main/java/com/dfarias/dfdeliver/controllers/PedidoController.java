8e80958723c55b4945ae437dc68e45d5b8ea26f7
DaviFarias7 <davic.farias7@gmail.com>
2022-03-23 15:36:49 -0300

    POST /pedido

--- backend/src/main/java/com/dfarias/dfdeliver/controllers/PedidoController.java
+++ backend/src/main/java/com/dfarias/dfdeliver/controllers/PedidoController.java
@@ -1,15 +1,20 @@
 package com.dfarias.dfdeliver.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.dfarias.dfdeliver.dto.PedidoDTO;
 import com.dfarias.dfdeliver.services.PedidoService;
+
 
 @RestController
 @RequestMapping(value = "/pedidos")
@@ -23,5 +28,13 @@ public class PedidoController {
 		List<PedidoDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
-
+	
+	@PostMapping
+	public ResponseEntity<PedidoDTO> insert(@RequestBody PedidoDTO dto) {
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
+	}
+	
 }
--- backend/src/main/java/com/dfarias/dfdeliver/services/PedidoService.java
+++ backend/src/main/java/com/dfarias/dfdeliver/services/PedidoService.java
@@ -1,5 +1,6 @@
 package com.dfarias.dfdeliver.services;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,8 +9,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.dfarias.dfdeliver.dto.PedidoDTO;
+import com.dfarias.dfdeliver.dto.ProdutoDTO;
 import com.dfarias.dfdeliver.entities.Pedido;
+import com.dfarias.dfdeliver.entities.Produto;
+import com.dfarias.dfdeliver.entities.StatusDoPedido;
 import com.dfarias.dfdeliver.repositories.PedidoRepository;
+import com.dfarias.dfdeliver.repositories.ProdutoRepository;
 
 @Service
 public class PedidoService {
@@ -17,10 +22,25 @@ public class PedidoService {
 	@Autowired
 	private PedidoRepository repository;
 	
+	@Autowired
+	private ProdutoRepository produtoRepository;
+	
 	@Transactional(readOnly = true)
 	public List<PedidoDTO> findAll(){
 		List<Pedido> list = repository.encontrarPedidosComProdutos();
 		return list.stream().map(x -> new PedidoDTO(x)).collect(Collectors.toList());
+	}
+	
+	@Transactional
+	public PedidoDTO insert(PedidoDTO dto){
+		Pedido pedido = new Pedido(null, dto.getEndereco(), dto.getLatitude(), dto.getLongitude(), 
+				Instant.now(), StatusDoPedido.PENDENTE);
+		for (ProdutoDTO p : dto.getProdutos()) {
+			Produto produto = produtoRepository.getOne(p.getId());
+			pedido.getProdutos().add(produto);
+		}
+		pedido = repository.save(pedido);
+		return new PedidoDTO(pedido);
 	}
 	
 }

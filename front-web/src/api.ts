950e86ae1b21b7306e3d16b2db9107912a643b9d
davi.farias <davi.farias@unimedjp.com.br>
2023-11-07 14:12:08 -0300

    Corrigindo bugs no front versao teste

--- front-web/src/api.ts
+++ front-web/src/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { OrderPayload } from "./Orders/Types";
 
-const API_URL = 'https://dfdeliver.herokuapp.com'; /*http://localhost:8080*/
+const API_URL = 'http://localhost:8080'; /*'https://dfdeliver.herokuapp.com'*/
 const mapboxToken = process.env.REACT_APP_ACCESS_TOKEN_MAP_BOX;
 
 export function fetchProducts() {
